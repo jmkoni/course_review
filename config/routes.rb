fdabd72d257b745dea03a92d1a840a833a062ac5
Jennifer Konikowski <jennifer.konikowski@stitchfix.com>
2019-06-01 16:08:46 -0400

    user can sign up and login

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   resources :users, only: %i[index create destroy]
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations'}, path: ''
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'application#home'
 end
