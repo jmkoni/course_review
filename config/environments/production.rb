b1274b9234d9bdbbf9adf8f7e81d673e6b7afa96
Jennifer Konikowski <jennifer.konikowski@stitchfix.com>
2019-07-14 10:17:47 -0400

    hook mailer up with sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,16 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { host: 'jmkoni.com' }
+  ActionMailer::Base.smtp_settings = {
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
+    address:              "smtp.sendgrid.net",
+    port:                 587,
+    enable_starttls_auto: true,
+    authentication:       :plain,
+    domain:               "jmkoni.com"
+  }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
